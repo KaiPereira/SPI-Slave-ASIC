3e0404a00606ba356302c8b1016e8ddba82cf399
Kai Pereira <kaipereira2020@gmail.com>
2026-09-07 01:51:00 -0400

    prepping my top file

--- src/icepi_top.v
+++ src/icepi_top.v
@@ -1,5 +1,23 @@
 module icepi_top (
-
+    input wire clk,
+    input wire [1:0] buttons,
+    input wire pi_mosi,
+    input wire pi_miso,
+    input wire pi_sclk,
+    input wire pi_ce0
 );
+    wire [7:0] uo_out;
+    wire [7:0] uio; // Tricky because both uio_in and uio_out
 
+
+    tt_um_kaipereira_spi_slave dut (
+        ui_in(8'b0),
+        uo_out(),
+        uio_in(),
+        uio_out(),
+        uio_oe(),
+        ena(),
+        clk(),
+        rst_n()
+    )
 endmodule
